b4f66435245c3a71d235fd8909f02d1c96e529dc
Artem Sirosh <asir2089@gmail.com>
2024-10-08 02:07:25 +0200

    Enable DB connection closing after insertion

--- src/add-markador.php
+++ src/add-markador.php
@@ -2,20 +2,21 @@
 
 require_once('data-access.php');
 
-if (isset($_POST['uri'])) {
-    $bm_uri = addslashes(trim($_POST['uri']));
-    if (mb_strlen($bm_uri) > 0) {
-        try {
+try {
+    if (isset($_POST['uri'])) {
+        $bm_uri = addslashes(trim($_POST['uri']));
+        if (mb_strlen($bm_uri) > 0) {
             $db = connect_db();
             add_bm($db, $bm_uri);
-        } catch (Exception $e) {
-            error_log('An error occured due to working with db:'.$e->getMessage());
+            $db->close();
+        } else {
+            error_log('Request param empty!');
         }
     } else {
-        error_log('Request param empty!');
+        error_log('HTTP method not POST');
     }
-} else {
-    error_log('HTTP method not POST');
+} catch (Exception $e) {
+    error_log('An error occured due to working with db:'.$e->getMessage());
 }
 
 
