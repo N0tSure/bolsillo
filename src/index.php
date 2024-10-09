20d1c65da926f899e0c540044c67c18d6906598b
Artem Sirosh <asir2089@gmail.com>
2024-10-09 21:33:44 +0200

    Change home page
    - Update page movement changed from link to button
    - Minor improvements

--- src/index.php
+++ src/index.php
@@ -37,6 +37,8 @@ try {
     halt($e->getMessage());
 }
 
+$db->close();
+
 echo '<table>';
 echo '<thead><tr>';
 echo '<th>Markador</th>';
@@ -44,19 +46,34 @@ echo '</tr></thead>';
 echo '<tbody>';
 foreach ($bms as $id => $bm) {
     echo '<tr>';
-    echo '<td><a href="' . $bm . '">' . htmlspecialchars($bm) . '</a></td>';
-    echo '<td><span><form action="delete-markador.php" method="POST">';
+
+    echo '<td>';
+    echo '<a href="' . $bm . '">' . htmlspecialchars($bm) . '</a>';
+    echo '</td>';
+
+    echo '<td>';
+    echo '<form action="delete-markador.php" method="POST">';
     echo '<input type="hidden" name="bm_id" id="bm_id" value="'.$id.'" />';
-    echo '<span><input type="submit" value="Delete" /></span>';
-    echo '</form></span></td>';
-    echo '<td><span><a href="/update-markador.php?id='.$id.'">Update</a></span></td>';
+    echo '<span>';
+    echo '<input type="submit" value="Delete" />';
+    echo '</span>';
+    echo '</form>';
+    echo '</td>';
+
+    echo '<td>';
+    echo '<form action="update-markador.php" method="GET">';
+    echo '<input type="hidden" name="id" id="id" value="'.$id.'" />';
+    echo '<span>';
+    echo '<input type="submit" value="Update" />';
+    echo '</span>';
+    echo '</form>';
+    echo '</td>';
+
     echo '</tr>';
 }
 
 echo '</tbody>';
 echo '</table>';
-
-$db->close();
 
 // Print footer
 print_footer();
