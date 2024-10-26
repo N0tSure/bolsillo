db434a2a14a33295edf372087610841a84b24114
Artem Sirosh <asir2089@gmail.com>
2024-10-26 14:57:14 +0200

    Add navbar component

---
+++ src/navbar.php
@@ -0,0 +1,18 @@
+<!-- Navigation -->
+<div id="navbar-panel" class="navbar">
+    <div>
+        <a  href="/">
+            <span>Home</span>
+        </a>
+    </div>
+    <div>
+        <a href="#">
+            <span>Change Password</span>
+        </a>
+    </div>
+    <div>
+        <a class="quit-btn" href="#">
+            <span>Logout</span>
+        </a>
+    </div>
+</div>
