5611d36022d468c596f54bd8b97c53441e591d15
Artem Sirosh <asir2089@gmail.com>
2024-10-26 16:24:39 +0200

    Add procedure for printing footer

--- src/common-helpers.php
+++ src/common-helpers.php
@@ -4,10 +4,16 @@
  * Checks that provided id is number.
  *
  * :param id string for validation
- * :retrun true if id is number
+ * :return true if id is number
  */
 function is_valid_id($id) {
     return preg_match('/^\d+$/', $id) === 1;
 }
 
-?>
+/*
+ * Prints HTML page footer
+ */
+function print_footer() {
+    echo '</body>';
+    echo '</html>';
+}
