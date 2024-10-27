6da83cd13668efc73c4782ceaa0d6c2c5217ace3
Artem Sirosh <asir2089@gmail.com>
2024-10-27 09:39:17 +0100

    Add User and UserService

---
+++ src/service.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * User model.
+ */
+class User
+{
+    /**
+     * Identifier. If null, this is new.
+     * @return int|null
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Name, never null.
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Email, never null.
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Makes this User existing
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    private $id;
+    private $name;
+    private $email;
+
+    public function __construct($name, $email)
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+}
+
+class UserService
+{
+    /**
+     * Finds User by login (email) and password. Returns only active user.
+     * If User not found, returns false.
+     *
+     * @param $login string login (email) not null
+     * @param $password string password, not null
+     * @return false|User
+     */
+   public function findUserByLoginAndPassword($login, $password)
+   {
+       $encrypted = md5($password);
+       if (
+           strcasecmp($login, 'ASir2089@gmail.com') == 0 &&
+           strcmp($encrypted, '6df23dc03f9b54cc38a0fc1483df6e21') == 0
+       ) {
+           $user = new User('Artem Sirosh', 'ASir2089@gmail.com');
+           $user->setId(1);
+           return $user;
+       }
+
+       return false;
+   }
+}
