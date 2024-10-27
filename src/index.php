c1f71c48a890ccec427930effd50561ccb25b677
Artem Sirosh <asir2089@gmail.com>
2024-10-27 22:30:43 +0100

    Introduce Marcador service
    Added MarcadorService into the home page. Changed page
    layout to rows and columns.

--- src/index.php
+++ src/index.php
@@ -21,72 +21,76 @@ echo '<div class="header">';
 echo '<h1>Welcome to Bolsillo!</h1>';
 echo '</div>';
 
-// Main content start
-echo '<div class="row">';
-echo '<div class="column">';
-
-require_once('data-access.php');
-$db = null;
+$marcadores = false;
 try {
-    $db = connect_db();
+    require_once 'service.php';
+    $svc = new MarcadorService();
+    $marcadores = $svc->getMarcadores(1);
 } catch (Exception $e) {
-    halt($e->getMessage());
-}
-
-$bms = null;
-try {
-    $bms = get_bm_list($db);
-    $db->close();
-} catch (Exception $e) {
-    $db->close();
     halt($e->getMessage());
 }
 
 // Add Marcador form
+echo '<div class="row">';
+echo '<div class="column">';
 echo '<div class="add-marcador main-buttons">';
 echo '<form action="add-markador.php" method="post">';
-echo '<label for="new_uri">Save a new Marcador to Bolsillo</label>';
+echo '<label class="marcadores-paragraph" for="new_uri">Save a new Marcador to Bolsillo</label>';
 echo '<input type="text" id="new_uri" name="uri" placeholder="Marcador URL"/>';
 echo '<input type="submit" value="Add"/>';
 echo '<input type="reset" value="Clear"/>';
 echo '</form>';
 echo '</div>';
-
-// Bookmarks
-echo '<div>';
-echo '<p>There are Marcadores that you saved.</p>';
-echo '</div>';
-echo '<div class="marcador-container">';
-
-foreach ($bms as $id => $bm) {
-    // Marcador
-    echo '<div class="marcador">';
-
-    // Marcador Link (anchor element)
-    echo '<a href="'.$bm.'">';
-    echo '<span>'.htmlspecialchars($bm).'</span>';
-    echo '</a>';
-
-    // Delete Marcador form
-    echo '<form class="main-buttons" action="delete-markador.php" method="POST">';
-    echo '<input type="hidden" name="bm_id" value="'.$id.'"/>';
-    echo '<input type="submit" value="Delete"/>';
-    echo '</form>';
-
-    // Update Marcador form
-    echo '<form class="main-buttons" action="update-marcador-form.php" method="GET">';
-    echo '<input type="hidden" name="id" value="'.$id.'"/>';
-    echo '<input type="submit" value="Update"/>';
-    echo '</form>';
-
-    echo '</div>';
-}
-
-echo '</div>';
-
-// Main content end
 echo '</div>';
 echo '</div>';
+
+// Bookmarks paragraph
+echo '<div class="row">';
+echo '<div class="column">';
+echo '<p class="marcadores-paragraph">';
+if ($marcadores):
+    echo 'There are Marcadores that you saved.';
+else:
+    echo 'There is no Marcadores yet.';
+endif;
+echo '</p>';
+echo '</div>';
+echo '</div>';
+
+if ($marcadores):
+    foreach ($marcadores as $i => $bm):
+
+        // Marcador Link (anchor element)
+        echo '<div class="row">';
+
+        echo '<div class="column-9-10">';
+        echo '<div class="marcador-container">';
+        echo '<a href="'.$bm->getUri().'">';
+        echo '<span>'.htmlspecialchars($bm->getUri()).'</span>';
+        echo '</a>';
+        echo '</div>';
+        echo '</div>';
+
+        echo '<div class="column-1-10">';
+        echo '<div class="marcador-container">';
+        // Delete Marcador form
+        echo '<form class="main-buttons" action="delete-markador.php" method="POST">';
+        echo '<input type="hidden" name="bm_id" value="'.$bm->getId().'"/>';
+        echo '<input type="submit" value="Delete"/>';
+        echo '</form>';
+
+        // Update Marcador form
+        echo '<form class="main-buttons" action="update-marcador-form.php" method="GET">';
+        echo '<input type="hidden" name="id" value="'.$bm->getId().'"/>';
+        echo '<input type="submit" value="Update"/>';
+        echo '</form>';
+
+        echo '</div>';
+        echo '</div>';
+
+        echo '</div>';
+    endforeach;
+endif;
 
 // Footer
 print_footer();
