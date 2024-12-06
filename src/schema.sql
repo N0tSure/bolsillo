f7a9096ad9e7fa23630fe4ca324531de8b55e1c0
Artem Sirosh <asir2089@gmail.com>
2024-12-06 13:45:55 +0100

    Add test data and updated schema

--- src/schema.sql
+++ src/schema.sql
@@ -1,17 +1,19 @@
--- Marcadores
-CREATE TABLE bookmark(
-	id INTEGER CONSTRAINT pk_bookmark PRIMARY KEY AUTOINCREMENT,
-	uri STRING NOT NULL
-);
-
 -- Users
 CREATE TABLE users(
-                      id INTEGER CONSTRAINT pk_users PRIMARY KEY AUTOINCREMENT,
-                      email TEXT NOT NULL,
-                      password TEXT NOT NULL,
-                      name TEXT NOT NULL,
-                      is_active BOOLEAN NOT NULL
+    id INTEGER CONSTRAINT pk_users PRIMARY KEY AUTOINCREMENT,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    name TEXT NOT NULL,
+    is_active BOOLEAN NOT NULL
 );
 
 -- Unique index
 CREATE UNIQUE INDEX uq_users_email ON users (email);
+
+-- Marcadores
+CREATE TABLE bookmarks(
+	id INTEGER CONSTRAINT pk_bookmark PRIMARY KEY AUTOINCREMENT,
+	uri STRING NOT NULL,
+    user INTEGER NOT NULL,
+    FOREIGN KEY (user) REFERENCES users(id)
+);
